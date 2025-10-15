e79b4259f5b566ed54277dbe21c4b79adfc9c63c
dnor-dev <anorue4rss@hotmail.com>
2025-10-15 17:53:40 +0100

    chore: sidebar and nav responsiveness

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,16 +1,26 @@
 import { HStack } from "../ui/stack";
 import { Input } from "../ui/input";
-import { SearchIcon, Video } from "lucide-react";
+import { SearchIcon, Video, AlignJustify } from "lucide-react";
 import { Button } from "../ui/button";
 import useDisclosure from "@/lib/hooks/use-disclosure";
 import CreateStream from "@/modules/stream/components/dialogs/create-stream";
+import MobileSidebar from "./mobile-sidebar";
+import { useState } from "react";
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <HStack className="pt-5 items-center justify-between">
-      <div className="bg-[#141414] w-[500px] !h-12 relative left-20">
+    <HStack className="pt-5 px-5 pb-5 items-center justify-between bg-[#141414] md:bg-inherit md:pb-0 -mx-5 md:mx-0 md:px-0 sticky md:static top-0 z-50 shadow-2xl">
+      <img
+        src="/assets/icons/wow-logo-full.svg"
+        alt="WOW"
+        className="w-[85px] h-[24px] md:hidden"
+      />
+
+      <div className="bg-[#141414] lg:w-[400px] xl:w-[500px] md:w-2/5 !h-12 relative lg:left-20 md:left-10 hidden md:block">
         <Input
           placeholder="Search"
           className="pl-10 border-none !bg-inherit !text-white !h-full !w-full"
@@ -18,7 +28,7 @@ const Navbar = () => {
         <SearchIcon className="text-[#5B5B5B] absolute left-2 top-1/2 -translate-y-1/2 placeholder:text-[#9F9F9F]" />
       </div>
 
-      <HStack className="space-x-5">
+      <HStack className="space-x-5 hidden md:flex">
         <Button className="bg-[#3A3A3A] rounded-[20px] w-fit text-sm font-semibold py-5">
           Log In
         </Button>
@@ -26,7 +36,16 @@ const Navbar = () => {
           Sign Up
         </Button>
         <Button
-          className="rounded-[20px] w-fit text-sm font-semibold py-5 pl-1"
+          className="rounded-full w-fit p-1 lg:hidden md:flex hidden"
+          onClick={onOpen}
+          title="Go Live"
+        >
+          <div className="w-[31px] h-[31px] rounded-full text-white flex items-center justify-center bg-[#141414]">
+            <Video />
+          </div>
+        </Button>
+        <Button
+          className="rounded-[20px] w-fit text-sm font-semibold py-5 pl-1 hidden lg:flex"
           onClick={onOpen}
         >
           <div className="w-[31px] h-[31px] rounded-full text-white flex items-center justify-center bg-[#141414]">
@@ -36,7 +55,19 @@ const Navbar = () => {
         </Button>
       </HStack>
 
+      <Button
+        variant="ghost"
+        size="icon"
+        className="md:hidden"
+        title="Open Menu"
+        onClick={() => setExpanded(true)}
+      >
+        <AlignJustify className="!w-7 !h-7" />
+      </Button>
+
       <CreateStream open={isOpen} onClose={onClose} />
+
+      <MobileSidebar expanded={expanded} setExpanded={setExpanded} />
     </HStack>
   );
 };
